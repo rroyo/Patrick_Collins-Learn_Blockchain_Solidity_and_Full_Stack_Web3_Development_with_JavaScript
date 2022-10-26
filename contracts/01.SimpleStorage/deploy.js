0e69d707329b5499eddb6db593414c6feef4b382
Ramon Royo <rroyog@uoc.edu>
2022-10-26 16:28:50 +0200

    sync

--- contracts/01.SimpleStorage/deploy.js
+++ contracts/01.SimpleStorage/deploy.js
@@ -5,6 +5,10 @@ async function main() {
   const provider = new ethers.providers.JsonRpcProvider(
     "http://127.0.0.1:7545"
   );
+  const wallet = new ethers.Wallet(
+    "84716e6724f7c0b4d7be9c480eaea10d42aa3adf959ca39a9fe69f3147cc102f",
+    provider
+  );
 }
 
 main()
