0927ce5559d7102eb79be7fad0e04750b0cfe0ed
Ramon Royo <rroyog@uoc.edu>
2022-11-21 07:46:35 +0100

    14:37

--- 9-hardhat-smart-contract-lottery/contracts/Raffle.sol
+++ 9-hardhat-smart-contract-lottery/contracts/Raffle.sol
@@ -10,10 +10,12 @@ pragma solidity ^0.8.7;
 
 import "@chainlink/contracts/src/v0.8/interfaces/VRFCoordinatorV2Interface.sol";
 import "@chainlink/contracts/src/v0.8/VRFConsumerBaseV2.sol";
+import "@chainlink/contracts/src/v0.8/AutomationCompatible.sol";
 
 error Raffle__NotEnoughETHEntered();
+error Raffle__TransferFailed();
 
-contract Raffle is VRFConsumerBaseV2 {
+contract Raffle is VRFConsumerBaseV2, AutomationCompatible {
   /* State Variables */
   uint256 private immutable i_entranceFee;
   address payable[] private s_players; // Winner has to be payable
@@ -24,11 +26,15 @@ contract Raffle is VRFConsumerBaseV2 {
   uint16 private constant REQUEST_CONFIRMATIONS = 3;
   uint16 private constant NUMBER_WORDS = 1;
 
+  // Lottery Variables
+  address private s_recentWinner;
+
   address immutable i_owner;
 
   /* Events */
   event RaffleEnter(address indexed player);
   event RaffleRequestedRaffleWinner(uint256 indexed requestId);
+  event RaffleWinnerPicked(address indexed winner);
 
   /* Modifiers */
   modifier onlyOwner() {
@@ -63,11 +69,24 @@ contract Raffle is VRFConsumerBaseV2 {
     emit RaffleEnter(msg.sender);
   }
 
+  /**
+   * @dev This is the function the Chainlink Keeper nodes call
+   * they look for the `upkeepNeeded` to return true.
+   * The following should be true in order to return true:
+   *  1. Our time interval should have passed
+   *  2. The lottery should have at least 1 player, and have some ETH
+   *  3. Our subscription is funded with LINK
+   *  4. The lottery should be in an "open" state
+   */
+  function checkUpkeep(bytes calldata) external view override returns(bool upkeepNeeded, bytes memory) {
+    upkeepNeeded = (block.timestamp - lasTimeStamp) > interval;
+  }
+
   // This function will be called by the Chainlink keepers network so that
   // this can automatically run without us having to interact with it
   // External functions are a little bit cheaper than public functions
   // since our own contract cannot call these
-  function requestRandomWinner() external onlyOwner returns (uint256) {
+  function requestRandomWinner() external {
     uint256 requestId = i_vrfCoordinator.requestRandomWords(
       i_gasLane,
       i_subscriptionId,
@@ -79,10 +98,19 @@ contract Raffle is VRFConsumerBaseV2 {
   }
 
   // This fuction is automatically called by requestRandomWinner()
-  function fulfillRandomWords(uint256 requestId, uint256[] memory randomWords) internal override {
+  function fulfillRandomWords(
+    uint256, /* requestId */ // We don't use this parameter, but this function needs a uint256 here
+    uint256[] memory randomWords
+  ) internal override {
     // We asked for a single random word, so we'll hard code the index of ranwomWords to 0
-    uint256 indexOfWinner = randomWords[0] % s_players.length;
+    uint256 indexOfWinner = randomWords[0] % s_players.length; // Returns the index of a player 
     address payable recentWinner = s_players[indexOfWinner];
+    s_recentWinner = recentWinner;
+    (bool success, ) = recentWinner.call{value: address(this).balance}("");
+    if (!success) {
+      revert Raffle__TransferFailed();
+    }
+    emit RaffleWinnerPicked(recentWinner);
   }
 
   /* View / Pure Functions */
@@ -92,5 +120,9 @@ contract Raffle is VRFConsumerBaseV2 {
 
   function getPlayer(uint256 index) public view returns (address) {
     return s_players[index];
+  }
+
+  function getRecentWinner() public view returns (address) {
+    return s_recentWinner;
   }
 }
